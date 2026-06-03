43e41a2b326937c5f1278d53a9351e10c918cd9d
23203046 <23203046@rmd.ac.in>
2026-06-03 16:16:51 +0530

    Convert html from submodule to folder

---
+++ html/Ex10/debug.js
@@ -0,0 +1,9 @@
+console.log("Community Portal Loaded");
+let totalUsers = 10;
+console.log("Total Users:",totalUsers);
+function testDebug(){
+let fee = 100;
+console.log("Event Fee:",fee);
+}
+
+testDebug();
